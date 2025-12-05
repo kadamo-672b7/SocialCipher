55f55ecb4da5c317d09a63c20e569aae02c1a1e4
kadamo-672b7 <e.gorlova.131756512@rambler.ru>
2025-12-05 10:35:36 +0800

    add responsive-inventory-serializer.java, environment-factory-resolver.rst, EthereumConfig.dbg.json

---
+++ frontend/web/src/components-model/responsive-download/message-import-database/responsive-inventory-serializer.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\components-model\responsive-download\message-import-database\responsive-inventory-serializer.java
+// Generated at: 2025-10-25T10:56:27.202Z
